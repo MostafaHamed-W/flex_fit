10047d41082f22bf7cca6a11436c0438c0c3ba97
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-08-22 04:32:47 +0300

    add background to welcome screeb

--- lib/pages/welcome_view.dart
+++ lib/pages/welcome_view.dart
@@ -1,10 +1,34 @@
+import 'package:flex_fit/shared/styles/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/route_manager.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: kThirdColor,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/black/9.jpg',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: Get.height,
+            width: Get.width,
+            color: kThirdColor.withOpacity(0.7),
+          )
+        ],
+      ),
+    );
   }
 }
